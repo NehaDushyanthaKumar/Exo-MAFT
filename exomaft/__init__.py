737db36c11a9d1d4da66c69bb4f56cc5564313fd
kmshariat <cast.shariat@gmail.com>
2025-08-08 12:43:15 +0600

    added cross corr file path

--- exomaft/__init__.py
+++ exomaft/__init__.py
@@ -1,3 +1,4 @@
 from .maft_combine import TransmissionSpectrumProcessor
 from .getlines import Linelist
 from .maft_feature_tracking import FeatureTracker
+from .maft_cross_correlation import CrossCorrelator
